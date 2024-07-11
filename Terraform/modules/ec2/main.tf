84c08446793f1d92fa52daf7e995d010c48638d4
ShahmirYousaf <shahmiryousaf2003@gmail.com>
2024-07-11 20:25:44 +0500

    Docker compose

--- Terraform/modules/ec2/main.tf
+++ Terraform/modules/ec2/main.tf
@@ -9,6 +9,13 @@ resource "aws_security_group" "Proj_Shahmir_instance_sg" {
   }
 
   ingress {
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
