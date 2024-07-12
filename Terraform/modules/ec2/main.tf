1fb852db99fcad744c2d2de42d49e36a1ccd72ef
ShahmirYousaf <shahmiryousaf2003@gmail.com>
2024-07-12 17:57:51 +0500

    Final File

--- Terraform/modules/ec2/main.tf
+++ Terraform/modules/ec2/main.tf
@@ -2,42 +2,42 @@ resource "aws_security_group" "Proj_Shahmir_instance_sg" {
   vpc_id = var.vpc_id
 
   ingress {
-    from_port   = 22
+    from_port   = 22  # SSH
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 9000
+    from_port   = 9000  # Jnekins
     to_port     = 9000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 3000
+    from_port   = 3000  # Development
     to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 8080
+    from_port   = 8080  # SonarQube
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 50000
+    from_port   = 50000  # jenkins agent
     to_port     = 50000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0
+    from_port   = 0  #allow all
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
